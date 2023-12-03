67d6dab471c3c20737684efe1c5ad97653237015
Mohammad-Alzoubi <alzoubi.mohmad94@gmail.com>
2023-12-03 12:34:20 +0300

    Product CRUD

---
+++ app/DataTables/ProductDataTable.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class ProductDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     * @return \Yajra\DataTables\EloquentDataTable
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($query){
+                $editBtn   = "<a href='".route('admin.products.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+                $deleteBtn = "<a href='".route('admin.products.destroy', $query->id)."' class='btn btn-danger ml-2 delete-item'><i class='far fa-trash-alt'></i></a>";
+                $moreBtn   = '<div class="dropdown dropleft d-inline">
+                    <button class="btn btn-primary dropdown-toggle ml-1" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-cog"></i>
+                    </button>
+                    <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 28px, 0px); top: 0px; left: 0px; will-change: transform;">
+                      <a class="dropdown-item has-icon" href="#"><i class="far fa-heart"></i> Image Gallery</a>
+                      <a class="dropdown-item has-icon" href="#"><i class="far fa-file"></i> Variants</a>
+                    </div>
+                  </div>';
+
+//                '.route('admin.products-image-gallery.index', ['product' => $query->id]).'
+//                '.route('admin.products-variant.index', ['product' => $query->id]).'
+
+                return $editBtn.$deleteBtn.$moreBtn;
+            })
+            ->addColumn('image', function ($query){
+                return $img = "<img width='100px' src='".asset($query->thumb_image)."'></img>";
+            })
+            ->addColumn('type', function ($query){
+                return match ($query->product_type) {
+                    'new_arrival'      => '<i class="badge badge-success">New Arrival</i>',
+                    'featured_product' => '<i class="badge badge-warning">Featured</i>',
+                    'top_product'      => '<i class="badge badge-info">Top Product</i>',
+                    'best_product'     => '<i class="badge badge-danger">Best Product</i>',
+                    default            => '<i class="badge badge-dark">None</i>',
+                };
+            })
+            ->addColumn('status', function($query){
+                if($query->status == 1){
+                    $button = '<label class="custom-switch mt-2">
+                        <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status" >
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }else {
+                    $button = '<label class="custom-switch mt-2">
+                        <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }
+                return $button;
+            })
+            ->rawColumns(['action', 'image', 'status', 'type'])
+            ->setRowId('id');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\Product $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Product $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('product-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(0)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     *
+     * @return array
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('image')->width(10 ),
+            Column::make('name'),
+            Column::make('price'),
+            Column::make('type')->width(100),
+            Column::make('status')->width(100),
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(200)
+                  ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'Product_' . date('YmdHis');
+    }
+}
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -40,10 +40,12 @@
             <!-- Ecommerce -->
             <li class="dropdown {{setActive([
                 'admin.vendor-profile.*',
+                'admin.products.*'
             ])}}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Ecommerce</span></a>
                 <ul class="dropdown-menu">
                     <li class="{{setActive(['admin.vendor-profile.*'])}}"><a class="nav-link" href="{{route('admin.vendor-profile.index')}}">Vendor Profile</a></li>
+                    <li class="{{setActive(['admin.products.*'])}}"><a class="nav-link" href="{{route('admin.products.index')}}">Product</a></li>
                 </ul>
             </li>
 
