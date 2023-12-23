8ee184dcdc6f80c3b4315dc824bb00b0046eb600
Mohammad-Alzoubi <alzoubi.mohmad94@gmail.com>
2023-12-23 14:16:21 +0300

    General_settings time zone and currency icons

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\GeneralSetting;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+
+        $generalSetting = GeneralSetting::first();
+
+        /** set time zone */
+        Config::set('app.timezone', $generalSetting->time_zone);
+
+        /** Share variable at all view */
+        View::composer('*', function($view) use ($generalSetting){
+            $view->with(['settings' => $generalSetting]);
+        });
     }
 }
--- resources/views/frontend/pages/product-detail.blade.php
+++ resources/views/frontend/pages/product-detail.blade.php
@@ -195,9 +195,9 @@
                             <a class="title" href="javascript:;">{{$product->name}}</a>
                             <p class="wsus__stock_area"><span class="in_stock">in stock</span> (167 item)</p>
                             @if(checkDiscount($product))
-                                <h4>${{$product->offer_price}} <del>${{$product->price}}</del></h4>
+                                <h4>{{$settings->currency_icon}}{{$product->offer_price}} <del>{{$settings->currency_icon}}{{$product->price}}</del></h4>
                             @else
-                                <h4>${{$product->price}}</h4>
+                                <h4>{{$settings->currency_icon}}{{$product->price}}</h4>
                             @endif
                             <p class="review">
                                 <i class="fas fa-star"></i>
@@ -724,7 +724,7 @@
                 year: {{date('Y', strtotime($product->end_date))}},
                 month: {{date('m', strtotime($product->end_date))}},
                 day: {{date('d', strtotime($product->end_date))}},
-                enableUtc: true
+                // enableUtc: true
             });
         })
     </script>
