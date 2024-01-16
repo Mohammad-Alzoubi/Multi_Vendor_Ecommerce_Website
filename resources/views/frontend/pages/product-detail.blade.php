5f172f2d3abbe9fa357af29b5c483b808b5be4e3
Mohammad-Alzoubi <alzoubi.mohmad94@gmail.com>
2024-01-16 08:55:36 +0300

    Modify product detail

--- resources/views/frontend/pages/product-detail.blade.php
+++ resources/views/frontend/pages/product-detail.blade.php
@@ -168,7 +168,7 @@
                             <div class="exzoom hidden" id="exzoom">
                                 <div class="exzoom_img_box">
                                     @if($product->video_link)
-                                    <a class="venobox wsus__pro_det_video" data-autoplay="true" data-vbtype="video"
+                                    <a class="venobox wsus__pro_det_video" data-autoplay="false" data-vbtype="video"
                                        href="{{$product->video_link}}">
                                         <i class="fas fa-play"></i>
                                     </a>
